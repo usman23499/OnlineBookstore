ecc9316c0be17178e2c8e02e2c9c5c83984be018
Muhammad Usman Hussian <139441819+developer1strayasoft@users.noreply.github.com>
2024-08-05 05:40:24 +0500

    Initial commit

---
+++ OnlineBook.Book.Core/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineBook.Book.Core
+{
+    public class Book
+    {
+        public string Title { get; private set; }
+        public string Author { get; private set; }
+        public string Title { get; private set; }
+        public string Title { get; private set; }
+    }
+}
