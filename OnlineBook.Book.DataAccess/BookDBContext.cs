09f74dde6940184449833ab42ef47c8e3e96a117
Muhammad Usman Hussian <64378519+usman23499@users.noreply.github.com>
2024-08-05 06:27:21 +0500

    updated

---
+++ OnlineBook.Book.DataAccess/BookDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineBook.Book.DataAccess.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineBook.Book.DataAccess
+{
+    public class BookDBContext: DbContext
+    {
+        public BookDBContext(DbContextOptions<BookDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<BookDAO> Books { get; set; }
+        public DbSet<OrderDAO> Orders { get; set; }
+    }
+}
